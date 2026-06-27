e32ee253961d493e94057c14d0b6821042065e69
joestar817 <joestar817@foxmail.com>
2026-06-27 19:42:49 +0800

    更新内置订阅源

--- app/src/main/java/io/legado/app/data/dao/RssSourceDao.kt
+++ app/src/main/java/io/legado/app/data/dao/RssSourceDao.kt
@@ -113,7 +113,7 @@ interface RssSourceDao {
     @Query("delete from rssSources where sourceUrl = :sourceUrl")
     fun delete(sourceUrl: String)
 
-    @Query("delete from rssSources where sourceGroup like 'legado'")
+    @Query("delete from rssSources")
     fun deleteDefault()
 
     @get:Query("select * from rssSources where sourceGroup is null or sourceGroup = ''")
