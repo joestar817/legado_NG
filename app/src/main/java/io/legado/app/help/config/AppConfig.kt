60ca090aeebab0950cadbc56d65595b534b18ffc
joestar817 <joestar817@foxmail.com>
2026-08-24 03:34:16 +0800

    调整阅读点击区域默认布局

--- app/src/main/java/io/legado/app/help/config/AppConfig.kt
+++ app/src/main/java/io/legado/app/help/config/AppConfig.kt
@@ -74,13 +74,13 @@ object AppConfig : SharedPreferences.OnSharedPreferenceChangeListener {
 
     var isEInkMode = getThemeModePref() == "3"
     var clickActionTL = appCtx.getPrefInt(PreferKey.clickActionTL, 2)
-    var clickActionTC = appCtx.getPrefInt(PreferKey.clickActionTC, 0)
+    var clickActionTC = appCtx.getPrefInt(PreferKey.clickActionTC, 2)
     var clickActionTR = appCtx.getPrefInt(PreferKey.clickActionTR, 1)
-    var clickActionML = appCtx.getPrefInt(PreferKey.clickActionML, 0)
+    var clickActionML = appCtx.getPrefInt(PreferKey.clickActionML, 2)
     var clickActionMC = appCtx.getPrefInt(PreferKey.clickActionMC, 0)
-    var clickActionMR = appCtx.getPrefInt(PreferKey.clickActionMR, 0)
+    var clickActionMR = appCtx.getPrefInt(PreferKey.clickActionMR, 1)
     var clickActionBL = appCtx.getPrefInt(PreferKey.clickActionBL, 2)
-    var clickActionBC = appCtx.getPrefInt(PreferKey.clickActionBC, 0)
+    var clickActionBC = appCtx.getPrefInt(PreferKey.clickActionBC, 1)
     var clickActionBR = appCtx.getPrefInt(PreferKey.clickActionBR, 1)
     var themeMode = getThemeModePref()
     var useDefaultCover = appCtx.getPrefBoolean(PreferKey.useDefaultCover, false)
@@ -113,25 +113,25 @@ object AppConfig : SharedPreferences.OnSharedPreferenceChangeListener {
                 appCtx.getPrefInt(PreferKey.clickActionTL, 2)
 
             PreferKey.clickActionTC -> clickActionTC =
-                appCtx.getPrefInt(PreferKey.clickActionTC, 0)
+                appCtx.getPrefInt(PreferKey.clickActionTC, 2)
 
             PreferKey.clickActionTR -> clickActionTR =
                 appCtx.getPrefInt(PreferKey.clickActionTR, 1)
 
             PreferKey.clickActionML -> clickActionML =
-                appCtx.getPrefInt(PreferKey.clickActionML, 0)
+                appCtx.getPrefInt(PreferKey.clickActionML, 2)
 
             PreferKey.clickActionMC -> clickActionMC =
                 appCtx.getPrefInt(PreferKey.clickActionMC, 0)
 
             PreferKey.clickActionMR -> clickActionMR =
-                appCtx.getPrefInt(PreferKey.clickActionMR, 0)
+                appCtx.getPrefInt(PreferKey.clickActionMR, 1)
 
             PreferKey.clickActionBL -> clickActionBL =
                 appCtx.getPrefInt(PreferKey.clickActionBL, 2)
 
             PreferKey.clickActionBC -> clickActionBC =
-                appCtx.getPrefInt(PreferKey.clickActionBC, 0)
+                appCtx.getPrefInt(PreferKey.clickActionBC, 1)
 
             PreferKey.clickActionBR -> clickActionBR =
                 appCtx.getPrefInt(PreferKey.clickActionBR, 1)
